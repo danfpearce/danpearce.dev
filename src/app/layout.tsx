e8fe65babaf57b50e17e44b3d94adee8128eff5b
dpearce <danfpearce@gmail.com>
2024-08-18 17:47:30 -0400

    replacing default og stuff and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `danpearce.dev blog`,
+  description: `A blog by Dan Pearce built with Next.js and ${CMS_NAME}.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
